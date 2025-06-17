e56905787c7eed7447b8c0ed7e6df62a2f725225
GIS-BM <gogointospace@gmail.com>
2025-06-17 15:19:19 +0900

    feat: 계정 정보 저장 기능(2가지 방식으로 구현), 로그인, 로그아웃 기능 구현

--- tablepick-mini-project/src/com/tablepick/test/GetLoginDataSession/GetLoginDataSessionUnitTest.java
+++ tablepick-mini-project/src/com/tablepick/test/GetLoginDataSession/GetLoginDataSessionUnitTest.java
@@ -1,24 +1,24 @@
-package com.tablepick.test.GetLoginData;
+package com.tablepick.test.GetLoginDataSession;
 
 import java.sql.SQLException;
 
 import com.tablepick.model.AccountVO;
 import com.tablepick.service.TablePickSerivceCommon;
+import com.tablepick.session.SessionManager;
 
-public class GetLoginDataUnit {
+public class GetLoginDataSessionUnitTest {
 // 로그인 데이터 다른 클래스에서 유지되는지 테스트
 // 테스트 성공
 	
 	public static void main(String[] args) {
 		AccountVO loginData = null;
 		try {
-			if(TablePickSerivceCommon.getInstance().login("owner04", "pw1234")!=null) {
+			if(TablePickSerivceCommon.getInstance().loginSessionManager("owner02", "pw1234")!=null) {
 				System.out.println("로그인 성공");
 			}
-			loginData = TablePickSerivceCommon.getInstance().getLoginData();
+			System.out.println("로그인된 데이터 가져오기 : "+SessionManager.getLoginDataSession());
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
-		System.out.println("로그인 데이터 값 출력 : " + loginData);
 	}
 }
