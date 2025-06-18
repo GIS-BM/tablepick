357f25ef06511b2d771416309ad7931282bebf9a
yey3hh <wateveryehwant@gmail.com>
2025-06-18 10:49:42 +0900

    feat: 식당 주인 예약자 조회 기능 구현 중

---
+++ tablepick-mini-project/src/com/tablepick/test/Restaurant/TestSelectReservationListByOwner.java
@@ -0,0 +1,27 @@
+package com.tablepick.test.Restaurant;
+
+import java.util.List;
+import java.util.Map;
+
+import com.tablepick.exception.NoReservationException;
+import com.tablepick.model.RestaurantDao;
+
+public class TestSelectReservationListByOwner {
+	public static void main(String[] args) throws NoReservationException {
+		// 식당 주인이 조회하는 예약 리스트
+		try {
+			RestaurantDao restaurantDao = new RestaurantDao();
+			String accountId = "owner01";
+			
+			List<Map<String, String>> reservationList = restaurantDao.checkMyRestaurantReservationList(accountId);
+			
+			for (int i = 0; i < reservationList.size(); i++) {
+				System.out.println(reservationList.get(i));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
