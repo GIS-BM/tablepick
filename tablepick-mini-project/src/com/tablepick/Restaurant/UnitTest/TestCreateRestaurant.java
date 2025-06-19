192286d49ee04a88759ca893987d5e3323b88d9a
khgeung <khgeung5357@gmail.com>
2025-06-19 17:52:31 +0900

    feat : 식당 주인 메소드 변경

--- tablepick-mini-project/src/com/tablepick/Restaurant/UnitTest/TestCreateRestaurant.java
+++ tablepick-mini-project/src/com/tablepick/Restaurant/UnitTest/TestCreateRestaurant.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 
 import com.tablepick.exception.NotFoundRestaurantException;
 import com.tablepick.model.AccountVO;
-import com.tablepick.model.RestaurantDao;
+import com.tablepick.model.OwnerDao;
 import com.tablepick.model.RestaurantVO;
-
+import com.tablepick.service.OwnerService;
 import com.tablepick.session.SessionManager;
 
 //식당 등록을 테스트하는 클래스 입니다.
@@ -43,7 +43,9 @@ public class TestCreateRestaurant {
 		String accountId = SessionManager.getLoginDataSession().getId();
 				
 
-		RestaurantDao dao = new RestaurantDao();
+		OwnerService service = new OwnerService();
+		
+		
 		ArrayList<String> restaurantType = new ArrayList<String>();
 		restaurantType.add("한식");
 		restaurantType.add("중식");
@@ -67,7 +69,7 @@ public class TestCreateRestaurant {
 
 		try {
 
-			if (dao.existRestaurant(accountId) == true) {
+			if (service.existRestaurant(accountId) == true) {
 				System.out.println("                          ");
 				System.out.println("이미 식당이 존재합니다. 새로 등록하시려면 기존 식당을 삭제하세요.");
 				System.out.println("                          ");
@@ -103,7 +105,7 @@ public class TestCreateRestaurant {
 				try {
 
 					System.out.println("식당이 등록되었습니다.");
-					System.out.println("내 식당 등록 번호 : " + dao.createRestaurant(vo1));
+					System.out.println("내 식당 등록 번호 : " + service.createRestaurant(vo1));
 				} catch (SQLException e) {
 
 					System.out.println(e.getMessage());
--- tablepick-mini-project/src/com/tablepick/service/OwnerService.java
+++ tablepick-mini-project/src/com/tablepick/service/OwnerService.java
@@ -1,108 +1,115 @@
 package com.tablepick.service;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 import com.tablepick.exception.AccountNotFoundException;
 import com.tablepick.exception.NoReservationException;
 import com.tablepick.exception.NotFoundMenuException;
 import com.tablepick.exception.NotFoundRestaurantException;
 import com.tablepick.exception.NotMatchedPasswordException;
-import com.tablepick.model.RestaurantDao;
+import com.tablepick.model.OwnerDao;
 import com.tablepick.model.RestaurantVO;
 
 //식당 주인의 Service 입니다.
 //동작을 담당하는 메소드들의 모음입니다.
 public class OwnerService {
-	
-	private RestaurantDao restaurantDao;
-	private String accountId;
-	private String password;
-	private RestaurantVO restaurantVO;
-	private int reservationIdx;
+
+	private OwnerDao ownerDao = new OwnerDao();
 	private int restaurantIdx;
-	private String newName;
-	private String newType;
-	private String newAddress;
-	private String newTel;
 	private int newSales;
-	private String name;
-	private int price;
+
+	public OwnerService() {
+		this.ownerDao = ownerDao;
+		this.restaurantIdx = restaurantIdx;
+		this.newSales = newSales;
+	}
+	
 
 	
 	
-	public void findAccount() throws NotMatchedPasswordException, AccountNotFoundException, SQLException {
-		restaurantDao.findAccount(accountId, password);
+	public void findAccount(String accountId, String password) throws NotMatchedPasswordException, AccountNotFoundException, SQLException {
+		ownerDao.findAccount(accountId, password);
 	}
 	
-	public void createRestaurant() throws SQLException {
-		restaurantDao.createRestaurant(restaurantVO);
+	public int createRestaurant(RestaurantVO restaurantVO) throws SQLException {
+		return ownerDao.createRestaurant(restaurantVO);
 	}
 	
-	public void findMyRestaurant() throws SQLException, NotFoundRestaurantException {
-		restaurantDao.findMyRestaurant(accountId);
+	public RestaurantVO findMyRestaurant(String accountId) throws SQLException, NotFoundRestaurantException {
+		return ownerDao.findMyRestaurant(accountId);
 		
 	}
-	public void deleteMyRestaurant() throws SQLException, NotMatchedPasswordException, AccountNotFoundException {
-		restaurantDao.deleteMyRestaurant(accountId,password);
-		
-	}
-	
-	public void existRestaurant() throws SQLException {
-		restaurantDao.existRestaurant(accountId);
+	public void deleteMyRestaurant(String accountId, String password) throws SQLException, NotMatchedPasswordException, AccountNotFoundException {
+		ownerDao.deleteMyRestaurant(accountId,password);
 		
 	}
 	
-	public void existRestaurantReservation() throws SQLException {
-		restaurantDao.existRestaurantReservation(reservationIdx);
-		
-	}
-	public void findMyRestaurantReservationList() throws NotFoundRestaurantException, SQLException {
-		restaurantDao.findMyRestaurantReservationList(accountId);
+	public boolean existRestaurant(String accountId) throws SQLException {
+		return ownerDao.existRestaurant(accountId);
 		
 	}
 	
-	public void findMyRestaurantAndSales() throws SQLException, NotFoundRestaurantException {
+	public void existRestaurantReservation(int reservationIdx) throws SQLException {
+		ownerDao.existRestaurantReservation(reservationIdx);
 		
-		restaurantDao.findMyRestaurantAndSales(accountId, restaurantIdx);
+	}
+	public List<Map<String, String>> findMyRestaurantReservationList(String accountId) throws NotFoundRestaurantException, SQLException {
+		return ownerDao.findMyRestaurantReservationList(accountId);
 		
 	}
 	
-	public void updateMyRestaurantInfoAndSales() throws SQLException, NotFoundRestaurantException {
-		restaurantDao.updateMyRestaurantInfoAndSales(accountId,  reservationIdx, newName, newType,
+	public List<Map<String, String>> findMyRestaurantAndSales(String accountId, int reservationIdx) throws SQLException, NotFoundRestaurantException {
+		
+		return ownerDao.findMyRestaurantAndSales(accountId, restaurantIdx);
+		
+	}
+	
+	public void updateMyRestaurantInfoAndSales(String accountId, int reservationIdx, String newName, String newType, String newAddress, String newTel) throws SQLException, NotFoundRestaurantException {
+		ownerDao.updateMyRestaurantInfoAndSales(accountId,  reservationIdx, newName, newType,
 				newAddress,  newTel);
 		
 	}
 	
 //	public void updateRestaurantSales() {
-//		restaurantDao.updateRestaurantSales( accountId,  reservationIdx,  newSales);
+//		ownerDao.updateRestaurantSales( accountId,  reservationIdx,  newSales);
 //	}
 	
-	public void createMenu() throws SQLException, NotFoundRestaurantException {
-		restaurantDao.createMenu(accountId, name, price);
+	public void createMenu(String accountId, String name, int price) throws SQLException, NotFoundRestaurantException {
+		ownerDao.createMenu(accountId, name, price);
 	}
 	
-	public void findMenu() throws SQLException, NotFoundRestaurantException {
-		restaurantDao.findMenu(accountId);
+	public List<Map<String, String>> findMenu(String accountId) throws SQLException, NotFoundRestaurantException {
+		return ownerDao.findMenu(accountId);
 	}
 	
-	public void findMenuDetail() throws NotFoundMenuException, SQLException, AccountNotFoundException, NotFoundRestaurantException {
-		restaurantDao.findMenuDetail(accountId, name);
+	public void findMenuDetail(String accountId, String name) throws NotFoundMenuException, SQLException, AccountNotFoundException, NotFoundRestaurantException {
+		ownerDao.findMenuDetail(accountId, name);
 	}
 	
-	public void updateMenu() throws SQLException, NotFoundMenuException, AccountNotFoundException, NotFoundRestaurantException {
-		restaurantDao.updateMenu(accountId, name, price);
+	public void updateMenu(String accountId, String name, int price) throws SQLException, NotFoundMenuException, AccountNotFoundException, NotFoundRestaurantException {
+		ownerDao.updateMenu(accountId, name, price);
 	}
 	
-	public void deleteMenu() throws SQLException, NotFoundMenuException, AccountNotFoundException, NotFoundRestaurantException {
-		restaurantDao.deleteMenu(accountId, name);
+	public void deleteMenu(String accountId, String name) throws SQLException, NotFoundMenuException, AccountNotFoundException, NotFoundRestaurantException {
+		ownerDao.deleteMenu(accountId, name);
 	}
 	
-	public void findMyRestaurantReview() throws SQLException, NotFoundRestaurantException {
-		restaurantDao.findMyRestaurantReview(accountId);
+	public List findMyRestaurantReview(String accountId) throws SQLException, NotFoundRestaurantException {
+		return ownerDao.findMyRestaurantReview(accountId);
 	}
 	
-	public void updateCustomerSale() throws NoReservationException, SQLException {
-		restaurantDao.updateCustomerSale(accountId, reservationIdx,  newSales);
+	public void updateCustomerSale(String accountId, int reservationIdx, int newSale) throws NoReservationException, SQLException {
+		ownerDao.updateCustomerSale(accountId, reservationIdx,  newSales);
+	}
+	
+	public List<Map<String, String>> findMyRestaurantReservationMostList(String accountId) throws NotFoundRestaurantException, SQLException {
+		return ownerDao.findMyRestaurantReservationMostList(accountId);
+	}
+
+	public Map<String, String> findSelectedCustomer(String accountId, int reservationIdx) throws SQLException, NoReservationException {
+		return ownerDao.findSelectedCustomer(accountId, reservationIdx);
 	}
 }
 
