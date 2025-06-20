90b65d3bbf5923f372e00f167eb102f70ae4ae07
yey3hh <wateveryehwant@gmail.com>
2025-06-20 19:29:47 +0900

    feat: 식당 정보 변경 시 기존 예약자 리스트 삭제 기능 구현

--- tablepick-mini-project/src/com/tablepick/model/OwnerDao.java
+++ tablepick-mini-project/src/com/tablepick/model/OwnerDao.java
@@ -369,7 +369,8 @@ public class OwnerDao {
 
 			String updateRestaurantInfoSql = "UPDATE restaurant SET name = ?, type = ?, address = ?, tel = ? WHERE account_id = ?";
 			String updateSalesSql = "UPDATE sales SET sales = 0 WHERE reserve_idx = ?";
-
+			
+			
 			pstmt = con.prepareStatement(updateRestaurantInfoSql);
 			pstmt.setString(1, newName);
 			pstmt.setString(2, newType);
@@ -384,14 +385,37 @@ public class OwnerDao {
 			pstmt.setInt(1, reservationIdx);
 
 			pstmt.executeUpdate();
+			
+			deleteMyReservationList(accountId, con);
+			
 			 con.commit(); // 정보 업데이트 내의 모든 세부 작업 정상 처리
 			 
 		} catch (Exception e) {
 			con.rollback();
 			throw e;
 		} finally {
-			DatabaseUtil.closeAll(pstmt, con);
+//			DatabaseUtil.closeAll(pstmt, con);
+			pstmt.close();
 		}
+	}
+	
+	public void deleteMyReservationList(String accountId, Connection con) throws SQLException, NotFoundRestaurantException {
+	    PreparedStatement pstmt = null;
+
+	    try {
+	    	con = DatabaseUtil.getConnection();
+	        RestaurantVO restaurantVO = findMyRestaurant(accountId); 
+	        int restaurantIdx = restaurantVO.getRestaurantId(); 
+
+
+	        String deleteSql = "DELETE FROM reserve WHERE restaurant_idx = ?";
+	        pstmt = con.prepareStatement(deleteSql);
+	        pstmt.setInt(1, restaurantIdx);
+	        pstmt.executeUpdate();
+
+	    } finally {
+	        DatabaseUtil.closeAll(pstmt, con);
+	    }
 	}
 
 	/**
